02fa0f1205acdaafa8b83a8891417fb842aebc26
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-17 14:17:22 +0100

    Created table, printed on page. Project completed!

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,5 +3,25 @@
 @section('title - page', 'Home - Trains')
 
 @section('main - content')
-    <h1>ciao</h1>
+
+    @foreach ($trains as $elem)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Numero treno</th>
+                    <th scope="col">Destinazione</th>
+                    <th scope="col">Orario di partenza</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+
+                    <td>{{ $elem->train_number }}</td>
+                    <td>{{ $elem->destination }}</td>
+                    <td>{{ $elem->departure_time }}</td>
+                </tr>
+            </tbody>
+        </table>
+    @endforeach
+
 @endsection
