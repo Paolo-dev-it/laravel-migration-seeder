4f41bbdd5a847de6275511516a647b3210e08b8a
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-01-17 12:58:27 +0100

    Created columns for database

---
+++ database/migrations/2023_01_17_114408_create_trains_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('train_number', 20);
+            $table->string('destination', 100);
+            $table->dateTime('departure_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title - page', 'Home - Trains')
+
+@section('main - content')
+    <h1>ciao</h1>
+@endsection
